f85e49b1fa234121778ad09c9d0add418bbde69a
李帮耀 <lhy_1314@yeah.net>
2020-10-31 02:16:05 +0800

    function complete

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-
+// 最大时间
+const limit = 99 * 60 * 60 + 99 * 60 + 60
 const vue = new Vue({
   el: '#app',
   data: {
     iptHour: '',
     iptMin: '',
-    iptSec: ''
+    iptSec: '',
+    fMin: 0,
+    fHour: 0,
+    fSec: 0,
+    total: 0,
+    timerId: '',
+    state: ''
   },
   methods: {
     getIpt (option) {
@@ -27,22 +34,75 @@ const vue = new Vue({
      */
     checkValue () {
       if (!this.iptHour && !this.iptMin && !this.iptSec) {
-        window.alert('请输入参数')
+        this.$message({
+          message: '请输入参数',
+          type: 'error'
+        })
         return false
       } else {
-        this.iptHour = this.iptHour === '' ? 0 : this.iptHour
-        this.iptMin = this.iptMin === '' ? 0 : this.iptMin
-        this.iptSec = this.iptSec === '' ? 0 : this.iptSec
+        // 处理空值
+        this.fHour = this.iptHour === '' ? 0 : parseInt(this.iptHour)
+        this.fMin = this.iptMin === '' ? 0 : parseInt(this.iptMin)
+        this.fSec = this.iptSec === '' ? 0 : parseInt(this.iptSec)
+        // 判断是否越界
+        this.total = this.fHour * 60 * 60 + this.fMin * 60 + this.fSec
+        if (this.total > limit) {
+          this.$message({
+            message: '参数过大，请缩小范围',
+            type: 'error'
+          })
+          return false
+        } else if (this.total === 0) {
+          this.$message({
+            message: '参数不能为0',
+            type: 'error'
+          })
+        }
+        // 处理非法输入
+        this.seconds2Standard(this.total)
         return true
       }
+    },
+    seconds2Standard (seconds) {
+      const sec = seconds % 60
+      seconds -= sec * 1
+      let min = seconds / 60
+      min = min % 60
+      seconds -= min * 60
+      const hour = seconds / 60 / 60
+      this.fHour = hour
+      this.fMin = min
+      this.fSec = sec
+      this.iptHour = hour < 10 ? '0' + hour : hour
+      this.iptMin = min < 10 ? '0' + min : min
+      this.iptSec = sec < 10 ? '0' + sec : sec
     },
     start () {
       if (!this.checkValue()) {
         return
+      } else if (this.state === 'playing') {
+        return
       }
-      let start
+      this.timerId = setInterval(this.countdown, 1000)
+    },
+    countdown () {
+      this.total -= 1
+      this.state = 'playing'
+      if (this.total === 0) {
+        clearInterval(this.timerId)
+        this.state = 'paused'
+        this.$nextTick(() => {
+          this.$notify({
+            message: '计时结束',
+            type: 'success'
+          })
+        })
+      }
+      this.seconds2Standard(this.total)
     },
     pause () { },
     reset () { }
   }
 })
+vue.__proto__.$notify = ELEMENT.Notification
+vue.__proto__.$message = ELEMENT.Message
